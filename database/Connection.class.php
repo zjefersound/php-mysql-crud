a3bf20001c5d06101692a2f1280baa9dd37de96e
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2023-07-03 18:40:29 -0300

    initial setup

---
+++ database/Connection.class.php
@@ -0,0 +1,30 @@
+<?php
+
+    class Connection{
+        private $usuario;
+        private $senha;
+        private $banco;
+        private $servidor;
+        private static $pdo;
+        
+        public function __construct(){
+            $this->servidor = "localhost";
+            $this->banco = "crud_produtos";
+            $this->usuario = "root";
+            $this->senha = "";
+        }
+        //host *eS3BRkrES2A4wW(
+        public function conectar(){
+            try{
+                if(is_null(self::$pdo)){
+                    self::$pdo = new PDO("mysql:host=".$this->servidor.";dbname=".$this->banco, $this->usuario, $this->senha);
+                }
+                return self::$pdo;
+            } catch (PDOException $ex) {
+                echo $ex->getMessage();
+            }
+        }
+        
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="description" content="" />
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors" />
+  <meta name="generator" content="Hugo 0.84.0" />
+  <title>Painel de controle</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="styles.css" rel="stylesheet" />
+  <link rel="stylesheet" href="./assets/feather-icons/feather.css">
 </head>
+
 <body>
-  <h1>CRUD PHP</h1>
-  
+  <main>
+    <?php include './components/sidebar.php'; ?>
+  </main>
+
+  <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+
+  <script src="sidebars.js"></script>
 </body>
 </html>
