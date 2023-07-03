e800224cb0adae8bc487c1f18fa913227ebc6ea5
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2023-07-03 18:42:19 -0300

    start product class

---
+++ classes/Produto.class.php
@@ -0,0 +1,33 @@
+<?php
+include_once "../database/Connection.class.php";
+
+class Produto
+{
+    private $con;
+    private $codigo_prd;
+    private $descricao_prd;
+    private $data_cadastro;
+    private $preco;
+    private $ativo;
+    private $unidade;
+    private $tipo_comissao;
+    private $codigo_ctg;
+    private $foto;
+
+    public function __construct()
+    {
+        $this->con = new Connection();
+    }
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
+    public function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+}
+
+?>
