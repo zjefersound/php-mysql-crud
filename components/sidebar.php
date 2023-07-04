1773c3e41bc0ae4b564b0d3d7319e96315ac3709
Jeferson Santos Moraes de Souza <62676057+zjefersound@users.noreply.github.com>
2023-07-04 02:12:21 -0300

    sidebar title

--- components/sidebar.php
+++ components/sidebar.php
@@ -6,7 +6,7 @@ function sidebar($menuAtivo)
 
   <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      <span class="fs-4">Sidebar</span>
+      <span class="fs-4">Portal Admin</span>
     </a>
     <hr />
     <ul class="nav nav-pills flex-column mb-auto">
